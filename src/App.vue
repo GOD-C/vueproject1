34a2eb737dbb55f73ec6c0bd6b8cde74822cca2b
GOD-C <1104254104@qq.com>
2018-11-01 20:51:34 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+   <h2>hahahhaha</h2>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+ 
+}
+</style>
